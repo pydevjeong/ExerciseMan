4f2c274999c096eeaa611157cb9b82a1865a6e1c
Jeong Hun Nam <njh3421@gmail.com>
2022-09-23 14:51:26 +0900

    Make Folder Structure

---
+++ src/components/SideSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SideSearch(){
+  return (
+    <div>
+      <h1>diqiwin</h1>
+    </div>
+  )
+}
+
+export default SideSearch
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  plugins: [
+    new Dotenv()
+  ],
+  
+  resolve: {
+    fallback: {
+        "fs": false,
+        "path": false ,
+        "os": false
+    }
+  }
+}
