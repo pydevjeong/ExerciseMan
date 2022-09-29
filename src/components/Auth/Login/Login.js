c68f6b817c993689e86f8bbea82072a369e53906
pydevjeong <njh3421@gmail.com>
2022-09-30 01:39:04 +0900

    Add Login Css, Fix component
    Handle Login component

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import './Login.css'
 
 function Login({
   onSubmit = async (data) => {
@@ -13,37 +15,48 @@ function Login({
   } = useForm();
 
   return (
+    <div className="form_container">
+      <div className="mainLogo">
+        <Link className="mainLink" to='/'>운동人</Link>
+      </div>
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="Id">아이디</label>
-      <input
-        id="Id"
-        type="text"
-        placeholder="아이디를 입력해주세여"
-        aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
-        {...register("id", {
-          required: "아이디은 필수 입력입니다."
-        })}
-      />
-      {errors.email && <small role="alert">{errors.email.message}</small>}
-      <label htmlFor="password">비밀번호</label>
-      <input
-        id="password"
-        type="password"
-        placeholder="****************"
-        aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
-        {...register("password", {
-          required: "비밀번호는 필수 입력입니다.",
-          minLength: {
-            value: 8,
-            message: "6자리 이상 비밀번호를 사용하세요.",
-          },
-        })}
-      />
-      {errors.password && <small role="alert">{errors.password.message}</small>}
-      <button type="submit" disabled={isSubmitting}>
-        로그인
-      </button>
+      <div className="idInput">
+        <label htmlFor="Id">아이디</label>
+        <input
+          id="Id"
+          type="text"
+          placeholder="아이디를 입력해주세여"
+          aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
+          {...register("id", {
+            required: "아이디은 필수 입력입니다."
+          })}
+        />
+        {errors.email && <small role="alert">{errors.email.message}</small>}
+      </div>
+      <div className="pwInput">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="****************"
+          aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
+          {...register("password", {
+            required: "비밀번호는 필수 입력입니다.",
+            minLength: {
+              value: 8,
+              message: "6자리 이상 비밀번호를 사용하세요.",
+            },
+          })}
+        />
+        {errors.password && <small role="alert">{errors.password.message}</small>}
+      </div>
+      <div className="submitBtn">
+        <button type="submit" disabled={isSubmitting}>
+          로그인
+        </button>
+      </div>
     </form>
+    </div>
   );
 }
 
