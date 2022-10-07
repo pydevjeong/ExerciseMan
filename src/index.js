b7b7c8ba5853e35c2853db4c37f62e5381472c00
Seongcheol Shin <tlstjdcjf980809@gmail.com>
2022-10-07 19:49:49 +0900

    add booking

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import Login from './components/Auth/Login/Login';
-import Register from './components/Auth/Register/Register'
-import MainPage from './components/MainPage/MainPage';
-import MapPage from './components/Maps/MapPage';
-import MainCommunity from './components/Community/MainCommunity'
-import ContactUs from './components/SubPage/ContactUs';
-
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import MainPage from "./components/MainPage/MainPage";
+import MapPage from "./components/Maps/MapPage";
+import MainCommunity from "./components/Community/MainCommunity";
+import ContactUs from "./components/SubPage/ContactUs";
+import BookingPopup from "./components/SubPage/BookingPopup";
 
 const router = createBrowserRouter([
   {
@@ -30,32 +30,36 @@ const router = createBrowserRouter([
     element: <MapPage />,
   },
   {
-    path:'/basketball',
-    element:<MainCommunity/>
+    path: "/basketball",
+    element: <MainCommunity />,
   },
   {
-    path:'/baseball',
-    element:<MainCommunity/>
+    path: "/baseball",
+    element: <MainCommunity />,
   },
   {
-    path:'/badminton',
-    element:<MainCommunity/>
+    path: "/badminton",
+    element: <MainCommunity />,
   },
   {
-    path:'/soccer',
-    element:<MainCommunity/>
+    path: "/soccer",
+    element: <MainCommunity />,
   },
   {
-    path:'/gym',
-    element:<MainCommunity/>
+    path: "/gym",
+    element: <MainCommunity />,
   },
   {
-    path:'/others',
-    element:<MainCommunity/>
+    path: "/others",
+    element: <MainCommunity />,
   },
   {
     path: "/contact_us",
     element: <ContactUs />,
+  },
+  {
+    path: "/booking_popup",
+    element: <BookingPopup />,
   },
 ]);
 
