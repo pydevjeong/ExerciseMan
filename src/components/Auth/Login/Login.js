4c1f66fe8a28989810c1f5c60df4fed10eb5c26e
Seongcheol Shin <tlstjdcjf980809@gmail.com>
2022-10-21 21:27:34 +0900

    Lodin css

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,24 +1,24 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import './Login.css'
-import axios from 'axios'
+import "./Login.css";
+import axios from "axios";
 
 function Login({
   onSubmit = async (data) => {
-    const {userId,password}=data
-    console.log(userId,password)
-    await axios.get('/user/login',{
-      userId:userId,
-      password:password
-    })
-    .then(res=>{
-      console.log('good',res)
-      localStorage.getItem("token")
-    })
-    .catch(err=>console.log(err))
+    const { userId, password } = data;
+    console.log(userId, password);
+    await axios
+      .get("/user/login", {
+        userId: userId,
+        password: password,
+      })
+      .then((res) => {
+        console.log("good", res);
+        localStorage.getItem("token");
+      })
+      .catch((err) => console.log(err));
   },
-}) 
-{
+}) {
   const {
     register,
     handleSubmit,
@@ -28,46 +28,66 @@ function Login({
   return (
     <div className="form_container">
       <div className="mainLogo">
-        <Link className="mainLink" to='/'>운동人</Link>
+        <Link className="mainLink" to="/">
+          운동人
+        </Link>
       </div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="idInput">
-        <label htmlFor="Id">아이디</label>
-        <input
-          id="Id"
-          type="text"
-          placeholder="아이디를 입력해주세요"
-          aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
-          {...register("userId", {
-            required: "아이디은 필수 입력입니다."
-          })}
-        />
-        {errors.email && <small role="alert">{errors.email.message}</small>}
+      <div className="loginMainContentes">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="idInput">
+            <label htmlFor="Id"></label>
+            <input
+              id="Id"
+              type="text"
+              placeholder="아이디"
+              className="textBox"
+              aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
+              {...register("userId", {
+                required: "아이디은 필수 입력입니다.",
+              })}
+            />
+            {errors.email && <small role="alert">{errors.email.message}</small>}
+          </div>
+          <div className="pwInput">
+            <label htmlFor="password"></label>
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호"
+              className="textBox"
+              aria-invalid={
+                !isDirty ? undefined : errors.password ? "true" : "false"
+              }
+              {...register("password", {
+                required: "비밀번호는 필수 입력입니다.",
+                minLength: {
+                  value: 3,
+                },
+              })}
+            />
+            {errors.password && (
+              <small role="alert">{errors.password.message}</small>
+            )}
+          </div>
+          <div className="submitBtn">
+            <button
+              type="submit"
+              className="submitBtnStyle"
+              disabled={isSubmitting}
+            >
+              로그인
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="pwInput">
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="*****"
-          aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
-          {...register("password", {
-            required: "비밀번호는 필수 입력입니다.",
-            minLength: {
-              value: 3,
-            },
-          })}
-        />
-        {errors.password && <small role="alert">{errors.password.message}</small>}
+      <div className="findId">아이디 찾기 / 비밀번호 찾기 / 회원가입</div>
+      <div className="btLine" />
+      <div className="snsLogin">
+        <div>페이스북</div>
+        <div>구글</div>
       </div>
-      <div className="submitBtn">
-        <button type="submit" disabled={isSubmitting}>
-          로그인
-        </button>
-      </div>
-    </form>
     </div>
   );
 }
 
-export default Login
+export default Login;
