2bada5a20974a40a49efa78e6c2c42e95f7b33a2
weed7968 <97829172+weed7968@users.noreply.github.com>
2022-10-30 21:21:02 +0900

    Auth 수정

--- src/components/Auth/FindAccount/FindAccount.js
+++ src/components/Auth/FindAccount/FindAccount.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./FindAccount.css";
 
 const FindAccount = () => {
   return (
-    <div className="form_container">
+    <div className="form_container_acc">
       <div className="mainLogo">
-        <Link className="mainLink" to='/'>운동人</Link>
+        <Link className="mainLink" to="/">
+          운동人
+        </Link>
+      </div>
+      <div className="findAccount">
+        이메일로 계정 찾기
+        <input
+          type="text"
+          className="inputAddr"
+          placeholder="이메일을 입력해주세요"
+        />
+        <button className="findAccBtn">전송</button>
       </div>
     </div>
   );
--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,29 +1,29 @@
 import { useForm } from "react-hook-form";
 import { Link, Navigate, useNavigate } from "react-router-dom";
-import "./Login.css"
-import axios from "axios"
+import "./Login.css";
+import axios from "axios";
 import setAuthorizationToken from "../../../utils/setAuthoriztionToken";
 
-function Login(){
-
+function Login() {
   const onSubmit = async (data) => {
-    const {userId,password}=data
-    console.log(userId,password)
-    await axios.post('http://15.165.205.17:8080/login',{
-      userId:userId,
-      password:password
-    })
-    .then(res=>{
-      console.log('good',res)
-      if(res.data){
-        localStorage.setItem("user",JSON.stringify(res.data))
-        setAuthorizationToken(JSON.stringify(res.data))
-      }
-      return res.data
-    })
-    .catch(err=>console.log(err))
-  }
-  
+    const { userId, password } = data;
+    console.log(userId, password);
+    await axios
+      .post("http://15.165.205.17:8080/login", {
+        userId: userId,
+        password: password,
+      })
+      .then((res) => {
+        console.log("good", res);
+        if (res.data) {
+          localStorage.setItem("user", JSON.stringify(res.data));
+          setAuthorizationToken(JSON.stringify(res.data));
+        }
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+  };
+
   // const logout=()=>{
   //   localStorage.removeItem("user")
   // }
@@ -36,66 +36,81 @@ function Login(){
     handleSubmit,
     formState: { isSubmitting, isDirty, errors },
   } = useForm();
-  const navigate=useNavigate()
-  const findAccountLink=()=>{
-    navigate('/findAccount')
-  }
-  const registerLink=()=>{
-    navigate('/register')
-  }
+  const navigate = useNavigate();
+  const findAccountLink = () => {
+    navigate("/findAccount");
+  };
+  const registerLink = () => {
+    navigate("/register");
+  };
 
   return (
-
     <div className="form_container">
       <div className="mainLogo">
-        <Link className="mainLink" to='/'>운동人</Link>
+        <Link className="mainLink" to="/">
+          운동人
+        </Link>
       </div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="idInput">
-        <label htmlFor="Id">아이디</label>
-        <input
-          id="Id"
-          type="text"
-          placeholder="아이디를 입력해주세요"
-          aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
-          {...register("userId", {
-            required: "아이디은 필수 입력입니다."
-          })}
-        />
-      </div>
-      <div className="pwInput">
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="*****"
-          aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
-          {...register("password", {
-            required: "비밀번호는 필수 입력입니다.",
-            minLength: {
-              value: 3,
-            },
-          })}
-        />
-      </div>
-      <div className="findId">아이디 찾기 / 비밀번호 찾기 / 회원가입</div>
-      <div className="btLine" />
-      <div className="snsLogin">
-        <div>페이스북</div>
-        <div>구글</div>
-      </div>
-      <div className="submitBtn">
-      <button onClick={findAccountLink}>계정 찾기</button>
-        
-        <button type="submit" disabled={isSubmitting}>
-          로그인
-        </button>
-        <button onClick={registerLink}>회원가입</button>
-      </div>
-    </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="loginMainContentes">
+          <div className="idInput">
+            <label htmlFor="Id"></label>
+            <input
+              id="Id"
+              type="text"
+              className="textBox"
+              placeholder="아이디"
+              aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
+              {...register("userId", {
+                required: "아이디은 필수 입력입니다.",
+              })}
+            />
+          </div>
+          <div className="pwInput">
+            <label htmlFor="password"></label>
+            <input
+              id="password"
+              type="password"
+              className="textBox"
+              placeholder="비밀번호"
+              aria-invalid={
+                !isDirty ? undefined : errors.password ? "true" : "false"
+              }
+              {...register("password", {
+                required: "비밀번호는 필수 입력입니다.",
+                minLength: {
+                  value: 3,
+                },
+              })}
+            />
+          </div>
+          <div className="submitBtn">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="submitBtnStyle"
+            >
+              로그인
+            </button>
+          </div>
+        </div>
+        <div className="findId">
+          <div className="findAcc" onClick={findAccountLink}>
+            계정 찾기
+          </div>
+          &nbsp;/&nbsp;
+          <div className="goRegi" onClick={registerLink}>
+            회원가입
+          </div>
+        </div>
+        <div className="btLine" />
+        <div className="snsLogin">
+          <div>페이스북</div>
+          <div>구글</div>
+        </div>
+      </form>
     </div>
   );
 }
 
-
-export default Login
+export default Login;
