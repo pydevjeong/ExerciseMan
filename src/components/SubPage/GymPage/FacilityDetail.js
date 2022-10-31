679a810c3ac46e3cf38965252967fe904da646d9
Jeong Hun Nam <njh3421@gmail.com>
2022-10-31 20:21:51 +0900

    Add Icons and css in facilityDetail Page

--- src/components/SubPage/GymPage/FacilityDetail.js
+++ src/components/SubPage/GymPage/FacilityDetail.js
@@ -5,6 +5,9 @@ import Header from "../../Layout/Header/Header";
 import styles from "./FacilityDetail.module.css";
 import Iframe from "react-iframe";
 import ModalPage from "../../../Modal/ModalPage";
+import bookmark from "../../../img/bookmark.PNG";
+import shareIcon from "../../../img/shareIcon.PNG";
+import freegymImg from "../../../img/freegymImg.jpg";
 
 const FacilityDetail = () => {
   // const params=useNavigate()
@@ -12,16 +15,16 @@ const FacilityDetail = () => {
   // console.log(info.state);
   const decode = decodeURI(info?.search);
   // console.log(decode);
-  const navigate=useNavigate()
-  const [isOpen,setIsOpen] =useState(false)
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const goCommunity=()=>{
-    navigate('/community')
-  }
-  const openPopup=()=>{
-    navigate('/booking_popup',{state:info.state})
-    setIsOpen(true)
-  }
+  const goCommunity = () => {
+    navigate("/community");
+  };
+  const openPopup = () => {
+    navigate("/booking_popup", { state: info.state });
+    setIsOpen(true);
+  };
   return (
     <>
       <Header />
@@ -30,24 +33,47 @@ const FacilityDetail = () => {
           <div>
             <div className={styles.title_con}>
               <h1>{info.state.name}</h1>
-              <button>북마크</button>
+              <div className={styles.fewIcons}>
+                <img src={bookmark} alt="" srcset="" />
+              </div>
             </div>
           </div>
-
-          <div className={styles.location_con}>
-            <h2>{info.state.location}</h2>
+          <div className={styles.imgAndOthers}>
+            <div className={styles.imgContainer}>
+              <img src={freegymImg} alt="" />
+            </div>
+            <div className={styles.info_con}>
+              <div className={styles.text_con}>
+                <h2>{info.state.location}</h2>
+                <h3>{info.state.tel}</h3>
+                <h3>⭐⭐⭐⭐⭐ 5.0</h3>
+              </div>
+              <div className={styles.share_con}>
+                <label htmlFor="">공유하기</label>
+                <img
+                  className={styles.shareIcons}
+                  src={shareIcon}
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <div className={styles.majorBtns}>
+                <button onClick={goCommunity}>커뮤니티</button>
+                <button onClick={openPopup}>예약하기</button>
+              </div>
+            </div>
           </div>
-          <button>공유버튼</button>
-          <div className={styles.tel_con}>
-            <h3>{info.state.tel}</h3>
-          </div>
-          <div>
-            <button onClick={goCommunity}>커뮤니티</button>
-            <button onClick={openPopup}>예약하기</button>
-          </div>
-          <div>
-            <p>시설정보</p>
-            <span>임시데이터 카카오 검색 API를 통해서 헬스장 설명 작성할 예정</span>
+          {/* 시설정보 */}
+          <div className={styles.explainCon}>
+            <div className={styles.detailIExplain}>
+            <h3>시설정보</h3>
+            </div>
+            <div className={styles.reviewCon}>
+            <h3>후기</h3>
+            </div>
+            <span>
+              임시데이터 카카오 검색 API를 통해서 헬스장 설명 작성할 예정
+            </span>
             <Iframe
               url="https://youtube.com/embed/uz6TUGCXe7c"
               width="500px"
