45aeff2c04fb955e25e4d4d76958774bf351e64d
pydevjeong <njh3421@gmail.com>
2022-12-14 03:06:15 +0900

    current change

--- src/components/SubPage/GymPage/NearFacilityList.js
+++ src/components/SubPage/GymPage/NearFacilityList.js
@@ -5,12 +5,7 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea,CardMedia } from "@mui/material";
 import { createSearchParams, useNavigate } from "react-router-dom";
 
-<<<<<<< HEAD
 const NearFacilityList = ({ facility_id,name, facilityId ,location, tel,gymPicture,vid  }) => {
-=======
-// 보여지는 개수를 정해두고 로딩이 뜨면서 추가적인 정보가 보이면서 창이 내려가게 만들기
-const NearFacilityList = ({ facility_id,name, facilityId ,location, tel,gymPicture,vid }) => {
->>>>>>> 34b6230e45c7fa41b2cfde5d7f147302332e7c23
   const navigate=useNavigate()
   const params={gym:`${name}`}
 
@@ -19,12 +14,7 @@ const NearFacilityList = ({ facility_id,name, facilityId ,location, tel,gymPictu
 
   const cardClicked=(e)=>{
     e.preventDefault();
-<<<<<<< HEAD
     navigate(`/facilityDetail?${createSearchParams(params)}`,{state:{"name":`${name}`,"id":`${facility_id}`,"location":`${location}`,"tel":`${tel}`,"gymPicture":`${gymPicture}`,"vid":`${vid}`}})
-=======
-    // 미완성 기능 -> 오류 생김 22/10/16 -> 완성 22/10/21
-    navigate(`/facilityDetail?${createSearchParams(params)}`,{state:{"name":`${name}`,"id":`${facility_id}`,"location":`${location}`,"tel":`${tel}`,"gymPicture":`${gymPicture}`,"vid":`${vid}`}})
->>>>>>> 34b6230e45c7fa41b2cfde5d7f147302332e7c23
   }
   
   const cardStyle = {
